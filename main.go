7124a4dfdadf3fd2deec43ead0665cdb16a1d9f9
Ingrid <ingrnor@gmail.com>
2021-03-05 14:16:52 +0100

    added elevator functions

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 import(
 	"time"
 	"fmt"
+	. "./elevio"
+	"fmt"
+	. "./config"
 )
 func timerDoor() {
 
@@ -14,59 +17,38 @@ func timerDoor() {
 func main() {
 	timerDoor()
 	fmt.Println("time out")
-/*
-	numFloors := 4
-
-	elevio.Init("localhost:15657", numFloors)
-
-	var d elevio.MotorDirection = elevio.MD_Up
-	//elevio.SetMotorDirection(d)
-
-	drv_buttons := make(chan elevio.ButtonEvent)
-	drv_floors := make(chan int)
-	drv_obstr := make(chan bool)
-	drv_stop := make(chan bool)
-
-	go elevio.PollButtons(drv_buttons)
-	go elevio.PollFloorSensor(drv_floors)
-	go elevio.PollObstructionSwitch(drv_obstr)
-	go elevio.PollStopButton(drv_stop)
-
-	//drive down if between floors
-	onInitBetweenFloors()
 
 	for {
 		select {
 		case a := <-drv_buttons:
 			fmt.Printf("%+v\n", a)
-			elevio.SetButtonLamp(a.Button, a.Floor, true)
+			SetButtonLamp(a.Button, a.Floor, true)
 
 		case a := <-drv_floors:
 			fmt.Printf("%+v\n", a)
 			if a == numFloors-1 {
-				d = elevio.MD_Down
+				d = MD_Down
 			} else if a == 0 {
-				d = elevio.MD_Up
+				d = MD_Up
 			}
-			elevio.SetMotorDirection(d)
+			SetMotorDirection(d)
 
 		case a := <-drv_obstr:
 			fmt.Printf("%+v\n", a)
 			if a {
-				elevio.SetMotorDirection(elevio.MD_Stop)
+				SetMotorDirection(MD_Stop)
 			} else {
-				elevio.SetMotorDirection(d)
+				SetMotorDirection(d)
 			}
 
 		case a := <-drv_stop:
 			fmt.Printf("%+v\n", a)
 			for f := 0; f < numFloors; f++ {
-				for b := elevio.ButtonType(0); b < 3; b++ {
-					elevio.SetButtonLamp(b, f, false)
+				for b := ButtonType(0); b < 3; b++ {
+					SetButtonLamp(b, f, false)
 				}
 			}
 		}
 	}
-	*/
 	
 }
