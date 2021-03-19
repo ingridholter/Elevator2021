ebd6bcef1260b8426443c464995606d71f66dcfc
Ingvild <ingvilrh@stud.ntnu>
2021-03-19 17:49:26 +0100

    FIX COSTFUNC

--- costFunc/costFunc.go
+++ costFunc/costFunc.go
@@ -1,6 +1,6 @@
 package costFunc
 
-import(
+import (
 	. "./config"
 )
 
@@ -8,55 +8,51 @@ import(
 //får inn knappetrykk
 //skal gi ut ny R=4x(heis*3)
 
-
 //returnerer 100,101,102 basert på heis som skal ta ordren
 func bestElevator(eOld elevStateArray, b Buttontype, f int) int {
 	var CostMap map[int]int
-	for elevNum:=0; elevNum<NUMELEVATORS; elevNum++{
-		CostMap[elevNum] = timeToServeRequest(eOld[elevNum],b,f)
-	} 
+	for elevNum := 0; elevNum < NUMELEVATORS; elevNum++ {
+		CostMap[elevNum] = timeToServeRequest(eOld[elevNum], b, f)
+	}
 	temp := CostMap[0]
 	for key, value := range CostMap {
-		
-		if (value < temp){
+
+		if value < temp {
 			temp = value
 		}
-		
+
 	}
 	return temp.key + 100
 }
 
-func timeToServeRequest(eOld elevState, b Buttontype, f int) int{
+func timeToServeRequest(eOld elevState, b Buttontype, f int) int {
 	var e Elevator = eOld
 	e.request[f][b] = 1
 
 	var arrivedAtRequest = 0
-		
+
 	var duration int = 0
 
-	switch(e.behaviour){
+	switch e.behaviour {
 	case EBidle:
-			e.dirn = requestChooseDirection(e)
-			if (e.dirn == EBstop){
-				return duration
-			}
+		e.dirn = requestChooseDirection(e)
+		if e.dirn == EBstop {
+			return duration
+		}
 	case EBmoving:
-		duration += TRAVELTIME/2
+		duration += TRAVELTIME / 2
 		e.floor += e.dirn
 	case EBdoorOpen:
-		duration -= DOOROPENTIME/2
+		duration -= DOOROPENTIME / 2
 	}
 
-
-	
-
-	for{
-		if(requestsShouldStop(e)){
+	for {
+		if requestsShouldStop(e) {
 			e = requestClearAtCurrentFloor(e)
-			if(inner_b == b && inner_f == f){ //////USIKKER: Erstatter funksjonskallet ifEqual fra D-koden
+			if inner_b == b && inner_f == f { //////USIKKER: Erstatter funksjonskallet ifEqual fra D-koden
 				arrivedAtRequest = 1
-				}
-			if(arrivedAtRequest){
+			}
+			if arrivedAtRequest {
 				return duration
 			}
 			duration += DOOROPENTIME
@@ -67,13 +63,10 @@ func timeToServeRequest(eOld elevState, b Buttontype, f int) int{
 	}
 }
 
-func RequestMatrix(eOld elevStateArray, b Buttontype, f int) AllRequests{
+func RequestMatrix(eOld elevStateArray, b Buttontype, f int) AllRequests {
 	//gives 100,101,102
-	id = bestElevator(eOld,b, f)
-
-	AllRequests
-}
-
-
-
-    
+	id := bestElevator(eOld, b, f)
+	elevatorIndex := id - 100
+	AllRequests[f][b+3*elevatorIndex] = true //rad, kolonne
+	return AllRequests
+}h
