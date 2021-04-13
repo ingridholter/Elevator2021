7dfee7eee7cccf6f34308463c5c0b43efdd21a35
Ingrid <ingrnor@gmail.com>
2021-04-13 20:21:39 +0200

    now working with redistributing order

--- main/main.go
+++ main/main.go
@@ -10,6 +10,7 @@ import (
 	. "main/elevatorDriver"
 	. "main/elevio"
 	"os"
+	"time"
 )
 
 func main() {
@@ -56,8 +57,7 @@ func main() {
 
 	numFloors := 4
 	Init(simport, numFloors)
-	fmt.Println("init state: ",Elevator.Behaviour)
-	
+
 	drvButtons := make(chan ButtonEvent)
 	drvFloors := make(chan int)
 	drvObstr := make(chan bool)
@@ -68,167 +68,13 @@ func main() {
 	go PollStopButton(drvStop)
 
 	chanNewOrder := make(chan ButtonEvent)
-	chanElevator := make(chan ElevState,1)
-	chanElevatorArray:= make(chan [NumElevators]ElevState,1)
-	lostId := make(chan int) //only one id lost a time
-	
-	
-	if Between {
-		fmt.Println("on init between floors")
-		OnInitBetweenFloors()
-	}
-	
-	go ElevatorObserver(id,ElevStateMsgRx,drvButtons,NewOrderMsgRx, NewOrderMsgTx,chanNewOrder,chanElevator,chanElevatorArray,ElevStateMsgTx, peerUpdateCh, lostId)
+	chanElevator := make(chan ElevState, 1)
+	chanElevatorArray := make(chan [NumElevators]ElevState, 1)
+	chanElevatorLastMoved := make(chan map[int]time.Time, 1)
+	lostId := make(chan int, 3) //only one id lost a time
 
-	go drvElevator(id,chanNewOrder,drvFloors,drvObstr,drvStop, chanElevator,ElevStateMsgTx)
+	go DrvElevator(id, chanNewOrder, drvFloors, drvObstr, drvStop, chanElevator, ElevStateMsgTx)
+	go ElevatorObserver(id, ElevStateMsgRx, drvButtons, NewOrderMsgRx, NewOrderMsgTx, chanNewOrder, chanElevator, chanElevatorArray, ElevStateMsgTx, peerUpdateCh, lostId, chanElevatorLastMoved)
 
-
-	select{}
+	select {}
 }
-/*
-
-
-	//Timer := time.NewTimer(2 * time.Second)
-	//Timer.Stop()
-
-	
-	//SendStateTicker := time.NewTicker(1*time.Second)
-
-	for i:= range ElevatorLastMoved{
-		ElevatorLastMoved[i] =  time.Now()
-	}
-	
-	//lostOrders := make(chan NewOrderMsg)
-	//go DistibuteLostOrders(id,powerloss)
-	//drive down if between floors
-	elevstate := ElevStateMsg{
-		SenderId: id,
-		Message:  "State Update",
-		Elevator: Elevator,
-	}
-	for {
-		select {
-		
-		case <-SendStateTicker.C:
-			//send my state every 1 seconds
-			
-			ElevStateMsgTx <- elevstate
-			
-
-		case p := <-peerUpdateCh:
-
-			fmt.Printf("Peer update:\n")
-			fmt.Printf("  Peers:    %q\n", p.Peers)
-			fmt.Printf("  New:      %q\n", p.New)
-			fmt.Printf("  Lost:     %q\n", p.Lost)
-
-			//evnt gjøre noe med new mtp software kræsj
-
-			//Ta lost sine cab orders og lagre dem. 
-
-			//når koblet på igjen: send msg om cab ordren til den som er new.
-
-			//i en annen case: ta new sin cab order kolonne og or'e med lagret cab orders
-
-			ActiveElevatorStates(p.Peers)
-			//fmt.Println("update active e: ", ElevStateArray)
-			
-		case l := <- lostOrders:
-			//update active elevators
-				
-				NewOrderMsgTx <- l
-		
-			//redistribuere alle ordre
-
-		case r := <-ElevStateMsgRx:
-			//fmt.Println("new msg from", r.SenderId)
-
-			
-			TimerElevatorLost(r,id,lostId,&_mtx3,&_mtx4)
-
-			UpdateElevStateArray(r,&_mtx4)
-			
-			SyncAllLights(ElevStateArray, id)
-			
-
-		case b := <-drv_buttons:
-			fmt.Printf("%+v\n", b)
-
-			msg := NewOrderDistributer(ElevStateArray, b.Button, b.Floor, id, Elevator,&_mtx4) //ny mld med hvem som skal ha ordre!
-			NewOrderMsgTx <- msg
-			
-
-		case m := <-NewOrderMsgRx:
-			fmt.Println("new order recieved: ", m)
-
-			if AcceptNewOrder(m, id, Elevator) { //is the new order for this elevator?
-				SyncAllLights(ElevStateArray, id)
-				OnRequestButtonPress(m.Button.Floor, m.Button.Button, Timer) //sets button request == true on wanted elevator
-			}
-			
-			elevstate = ElevStateMsg{
-				SenderId: id,
-				Message:  "State Update",
-				Elevator: Elevator,
-			} //oppdatere requests basert på knappetrykk
-			
-			
-		case a := <-drv_floors:
-			fmt.Printf("floor: %+v\n", a)
-			//Reset timer for powerloss
-
-			OnFloorArrival(a, id, Timer)
-			SyncAllLights(ElevStateArray, id)
-
-			
-			
-			elevstate = ElevStateMsg{
-				SenderId: id,
-				Message:  "State Update",
-				Elevator: Elevator, //sende over min state hele tiden!
-			}
-			
-		case a := <-drv_obstr:
-			fmt.Printf("Obstuction! %+v\n", a)
-			if a && Elevator.Behaviour==EBdoorOpen{
-				Timer.Reset(3*time.Second)
-				Timer.Stop()
-			}
-
-			if !a{
-				Timer.Stop()
-				OnDoorTimeOut()
-			}
-			
-		case <-Timer.C:
-			OnDoorTimeOut()
-
-		case id:= <-lostId:
-			//alarm
-			fmt.Println("In lostId: ", id)
-			DistibuteLostOrders(id,&_mtx4,NewOrderMsgTx)
-
-
-		case a := <-drv_stop:
-			fmt.Printf("stop button: %+v\n", a)
-			SetStopLamp(true)
-			for a{
-				for f := 0; f < numFloors; f++ {
-					for b := ButtonType(0); b < 3; b++ {
-					SetButtonLamp(b, f, false)
-					}				
-				}
-				time.Sleep(500*time.Millisecond)
-				for f := 0; f < numFloors; f++ {
-					for b := ButtonType(0); b < 3; b++ {
-					SetButtonLamp(b, f, true)
-
-					}
-				}
-				time.Sleep(500*time.Millisecond)
-			}
-		}
-	}
-
-}
-*/
